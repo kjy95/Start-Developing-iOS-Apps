710ad93c5fcd86b19fea609d34761dcb6faba594
Kim Ji Yeoung <cheld414@daum.net>
2019-07-01 17:20:58 +0900

    create foodtracker

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Vimosoft on 01/07/2019.
+//  Copyright © 2019 kjy95. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
